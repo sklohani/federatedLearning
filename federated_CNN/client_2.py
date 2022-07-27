ed54bfed63ce704f5bbbf6bf14978e187c876c2b
sklohani <s.shobhit9901@gmail.com>
2022-07-28 02:29:07 +0530

    Federated ANN & CNN

---
+++ federated_CNN/client_2.py
@@ -0,0 +1,87 @@
+## Import Modules
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
+import flwr as fl
+from sklearn.preprocessing import MinMaxScaler
+import scipy.io as sio
+import sys
+
+## Method to get test data
+def getTestDataset():
+    database = sio.loadmat('../testData/data_base_all_sequences_random.mat')
+
+    x = database['Data_test_2']
+    y = database['label_test_2']
+
+    return x, y
+
+## Import train data
+data = pd.read_csv("../data/data/node02/data.csv", index_col=0)
+x_train = data[data.columns[0:512]]
+y_train = data[data.columns[-1]]
+
+## Import test data
+x_test, y_test = getTestDataset()
+y_test = np.reshape(y_test, (y_test.shape[0],))
+
+## Scale train and test data
+scaler = MinMaxScaler()
+scaler_model = scaler.fit(x_test)
+x_train_scaled = scaler_model.transform(x_train)
+x_test_scaled = scaler_model.transform(x_test)
+
+## Creating Model
+model = tf.keras.models.Sequential([
+    layers.Reshape((512,1), input_shape=(512,1)),
+    layers.Conv1D(16, 3, activation='relu'),
+    layers.MaxPooling1D(3),
+    layers.Conv1D(4, 3, activation='relu'),
+    layers.Flatten(),
+    layers.Dense(8, activation='softmax')
+])
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+model.build(input_shape=(512, 1))
+
+accuracy_list = []
+loss_list = []
+
+## Flower Client
+class CifarClient(fl.client.NumPyClient):
+    def __init__(self):
+        self.totalRowCount = data.shape[0]
+        self.currentIndex = 0
+        self.continousTrainingBatchSize = 60
+        self.nextIndex = self.currentIndex + self.continousTrainingBatchSize
+
+    def get_parameters(self):
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(x_train_scaled[self.currentIndex:self.nextIndex], y_train[self.currentIndex:self.nextIndex], epochs=1)
+        self.currentIndex = self.nextIndex
+        self.nextIndex = self.currentIndex + self.continousTrainingBatchSize if self.currentIndex + self.continousTrainingBatchSize < self.totalRowCount else self.totalRowCount
+        return model.get_weights(), len(x_train_scaled), {}
+    
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test_scaled, y_test)
+        loss_list.append(loss)
+        accuracy_list.append(accuracy)
+        return loss, len(x_test_scaled), {"Accuracy": accuracy}
+
+## Start Client
+server_address='localhost:'+str(sys.argv[1])
+fl.client.start_numpy_client(server_address, client=CifarClient())
+
+## Writing results to file
+accuracy_list = np.array(accuracy_list)
+loss_list = np.array(loss_list)
+np.savetxt('./results/accuracy_client_2.txt', accuracy_list)
+np.savetxt('./results/loss_client_2.txt', loss_list)
---
+++ federated_CNN/server.py
@@ -0,0 +1,82 @@
+## Import Modules
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
+import flwr as fl
+from sklearn.preprocessing import MinMaxScaler
+import scipy.io as sio
+import sys
+
+## Method to get validation data
+def getTestDataset():
+    database = sio.loadmat('../testData/data_base_all_sequences_random.mat')
+
+    x = database['Data_test_2']
+    y = database['label_test_2']
+
+    return x, y
+
+## Import validation data
+x, y = getTestDataset()
+y = np.reshape(y, (y.shape[0],))
+x_val = x[5000:5500]
+y_val = y[5000:5500]
+
+## Scale validation data
+scaler = MinMaxScaler()
+scaler_model = scaler.fit(x_val)
+x_val = scaler_model.transform(x_val)
+
+
+## Load and compile model for server-side parameter evaluation
+model = tf.keras.models.Sequential([
+    layers.Reshape((512,1), input_shape=(512,1)),
+    layers.Conv1D(16, 3, activation='relu'),
+    layers.MaxPooling1D(3),
+    layers.Conv1D(4, 3, activation='relu'),
+    layers.Flatten(),
+    layers.Dense(8, activation='softmax')
+])
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+model.build(input_shape=(512, 1))
+
+accuracy_list = []
+loss_list = []
+
+# Centralized Evaluation for Server's Strategy
+def get_eval_fn(model):
+    '''Return an evaluation function for server side evaluation'''
+
+    def evaluate(weights: fl.common.Weights):
+        model.set_weights(weights)
+        loss, accuracy = model.evaluate(x_val, y_val)
+        loss_list.append(loss)
+        accuracy_list.append(accuracy)
+        return loss, {"Accuracy": accuracy}
+
+    return evaluate     
+
+strategy = fl.server.strategy.FedAvg( 
+    min_fit_clients = 5,
+    min_eval_clients = 5,
+    min_available_clients = 5,  
+    eval_fn = get_eval_fn(model)
+)
+
+# Start Server
+fl.server.start_server(
+    strategy = strategy,
+    server_address='localhost:'+str(sys.argv[1]),
+    config={"num_rounds": 54}
+)
+
+## Writing results to file
+accuracy_list = np.array(accuracy_list)
+loss_list = np.array(loss_list)
+np.savetxt('./results/accuracy_server.txt', accuracy_list)
+np.savetxt('./results/loss_server.txt', loss_list)
